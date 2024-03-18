2a57ed0d1a361d91ae665e4ea99af786a6da3837
Hoangsharksuy <hduyshark@gmail.com>
2024-03-18 08:42:38 +0700

    Add project files.

---
+++ jkhk.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+	float chieu_dai, chieu_rong, dien_tich;
+
+	cout << "Nhap vao chieu dai hinh chu nhat: \n";
+		cin >> chieu_dai;
+
+	cout << "Nhap vao chieu rong hinh chu nhat: \n";
+		cin >> chieu_rong;
+
+	dien_tich = chieu_dai * chieu_rong; // Công thức tính diện tích
+
+	cout << "Dien tich hinh chu nhat la: \n" << dien_tich << endl;
+
+		return 0;
+}
